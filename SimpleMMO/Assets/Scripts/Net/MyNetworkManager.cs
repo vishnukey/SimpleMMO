f1cecd9b633c2b9370158180fcf8b87989131447
PeterFajner <fajner1+Github@gmail.com>
2015-07-20 22:57:01 -0600

    Multiplayer movement and colour changing implemented using new Unity 5.1 Networking
    A simple movement and timed colour change behaviour implemented to test
    Unity's new networking system.

---
+++ SimpleMMO/Assets/Scripts/Net/MyNetworkManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager 
+{
+	public List<GameObject> players;
+
+	void Start ()
+	{
+		players = new List<GameObject>(); 
+		InvokeRepeating("ChangeColours", 1, 2F);
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+		players.Add(player);
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+	}
+
+	void ChangeColours()
+	{
+		foreach (GameObject player in players)
+		{
+			if (player == null)
+			{
+				players.Remove(player);
+				continue;
+			}
+			player.GetComponent<Player>().colour = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+		}
+	}
+}
---
+++ SimpleMMO/Assets/Scripts/Player.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    int moveX = 0;
+    int moveY = 0;
+	public float speed = 10f;
+	private Rigidbody rigidbody;
+	public GameObject gameObject;
+
+	[SyncVar]
+	public Color colour;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidbody = gameObject.GetComponent<Rigidbody>();
+		colour = new Color(0.5f, 0.5f, 0.5f);
+		GetComponent<Renderer>().material.color = colour;
+		InvokeRepeating("UpdateColour", 1f, 0.5f);
+	}
+
+	void UpdateColour()
+	{
+		GetComponent<Renderer>().material.color = colour;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        DoMovement();
+	}
+
+    //[Command]	
+	void DoMovement()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+		print("Moving");
+
+		/*
+        int oldMoveX = moveX;
+        int  oldMoveY = moveY;
+
+        if (Input.GetKey(KeyCode.W)) {
+            moveY += 1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            moveY -= 1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            moveX -= 1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            moveX += 1;
+        }
+
+        if (moveX != oldMoveX || moveY != oldMoveY) {
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * moveY * Time.deltaTime);
+			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * moveX * Time.deltaTime);
+
+        }
+		*/
+
+		if (Input.GetKey(KeyCode.W))
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.S))
+			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.D))
+			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.A))
+			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+    }
+}
